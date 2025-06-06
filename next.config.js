06afe29b2542fae322259615583a98b7d78477e0
aarush0101 <141508418+aarush0101@users.noreply.github.com>
2025-06-06 17:36:01 +0530

    feat(security): change security headers for both cf and nextjs conf

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value: [
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://firebase.googleapis.com https://www.gstatic.com/ https://fonts.gstatic.com/ https://fonts.googleapis.com https://www.googleapis.com https://static.cloudflareinsights.com;",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://cloudflareinsights.com https://www.google-analytics.com https://firebase.googleapis.com https://www.gstatic.com/ https://fonts.gstatic.com/ https://fonts.googleapis.com https://www.googleapis.com https://static.cloudflareinsights.com;",
               "object-src 'none';",
               "base-uri 'self';",
               "form-action 'self';",

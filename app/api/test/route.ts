2d4627fe9783bc56fe0088f8db293db15d83afc0
aarush0101 <141508418+aarush0101@users.noreply.github.com>
2025-06-07 16:04:57 +0530

    test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,8 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getEnvTest } from "@/lib/test";
 export const runtime = "edge";
 
 export async function GET() {
   const env = getEnvTest();
-  return NextResponse.json(
-    { resp: env },
-    { status: 200 }
-  );
+  return NextResponse.json({ resp: env }, { status: 200 });
 }

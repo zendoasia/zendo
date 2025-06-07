2ef7f6dfd0d990812696037771d21190827318a3
aarush0101 <riyansarma76@gmail.com>
2025-06-07 22:06:22 +0530

    refactor(minify): migrate to ES module syntax

--- static/minify.js
+++ static/minify.js
@@ -1,23 +1,26 @@
-const fs = require("fs");
-const Terser = require("terser");
+import fs from "fs";
+import { minify } from "terser";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-(async () => {
-  const swPath = path.resolve(__dirname, "../public/sw.js");
-  const minPath = path.resolve(__dirname, "../public/sw.min.js");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-  const code = fs.readFileSync(swPath, "utf-8");
-  const result = await Terser.minify(code, {
-    compress: true,
-    mangle: true,
-  });
+const swPath = path.resolve(__dirname, "../public/sw.js");
+const minPath = path.resolve(__dirname, "../public/sw.min.js");
 
-  fs.writeFileSync(minPath, result.code);
-  fs.unlinkSync(swPath);
+const code = fs.readFileSync(swPath, "utf-8");
+const result = await minify(code, {
+  compress: true,
+  mangle: true,
+});
 
-  // Remove itself
-  const staticDir = path.resolve(__dirname, "./static");
-  if (fs.existsSync(staticDir)) {
-    fs.rmSync(staticDir, { recursive: true, force: true });
-  }
-})();
+fs.writeFileSync(minPath, result.code);
+fs.unlinkSync(swPath);
+
+// Remove itself
+const staticDir = path.resolve(__dirname, "./static");
+if (fs.existsSync(staticDir)) {
+  fs.rmSync(staticDir, { recursive: true, force: true });
+}
